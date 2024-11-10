5dfba356a37de448ddf1f7ea5de30b14e4e5d4a2
Vinicius Reis <vinicius.reis@smartnx.com>
2024-11-10 20:40:21 -0300

    adiciona teste 'cesta inexistente'

--- test/unit/coupon.test.js
+++ test/unit/coupon.test.js
@@ -8,26 +8,30 @@ let response;
 
 beforeEach(() => {
   req = { params: {} };
-  res = { status: jest.fn().mockReturnThis(), send: jest.fn((data) => console.log('erro', data)) };
+  res = { status: jest.fn().mockReturnThis() };
 
   response = new Promise(resolve => {
     res.json = jest.fn((data) => resolve(data));
   });
   
   error = new Promise(resolve => {
-    next = jest.fn((data) => resolve(data));
+    res.send = jest.fn((data) => resolve(data));
   });
+
+  rejection = new Promise(resolve => {
+    next = jest.fn((data) => resolve(data));
+  })
 });
 
-describe('[ID: 07] - Módulo "Cupom de desconto"', () => {
-  describe('Casos de sucesso', () => {
-    test('Teste unitário para o método generateCoupon', async () => {
+describe('Módulo "Cupom de desconto"', () => {
+  describe('[ID: 07] - Aplicação de Cupom Válido na Cesta', () => {
+    test('Teste unitário para o método generateCoupon - Desconto válido', async () => {
       const coupon = security.generateCoupon('10')
       const decodedCoupon = z85.decode(coupon).toString()
       expect(decodedCoupon).not.toBeNull();
     });
     
-    test('Teste unitário para o método discountFromCoupon', async () => {
+    test('Teste unitário para o método discountFromCoupon - Cupom valido', async () => {
       req.params = { 
         id: '1', 
         coupon: security.generateCoupon('10')
@@ -37,10 +41,9 @@ describe('[ID: 07] - Módulo "Cupom de desconto"', () => {
       expect(discount).toBe(10);
     });
   
-    test('Teste para a funcionalidade discountFromCoupon', async () => {
+    test('Teste para a funcionalidade discountFromCoupon - Fluxo normal', async () => {
       const mockBasket = { update: jest.fn().mockResolvedValue() };
       BasketModel.findByPk = jest.fn().mockResolvedValue(mockBasket);
-      
       
       req.params = { 
         id: '1', 
@@ -58,13 +61,13 @@ describe('[ID: 07] - Módulo "Cupom de desconto"', () => {
     });
   })
   
-  describe('Casos de Erro', () => {
-    test('Teste unitário para o método generateCoupon - desconto inválido', async () => {
+  describe('[ID: 08] - Tentativa de Aplicação de Cupom Inválido', () => {
+    test('Teste unitário para o método generateCoupon - Desconto inválido', async () => {
       const coupon = security.generateCoupon('INVALID_DISCOUNT')
       expect(coupon).toBeNull();
     });
     
-    test('Teste unitário para o método discountFromCoupon - cupom inválido', async () => {
+    test('Teste unitário para o método discountFromCoupon - Cupom inválido', async () => {
       req.params = { 
         id: '1', 
         coupon: 'INVALID_COUPON'
@@ -72,6 +75,39 @@ describe('[ID: 07] - Módulo "Cupom de desconto"', () => {
   
       const discount = security.discountFromCoupon(req.params.coupon);
       expect(discount).toBeUndefined();
+    });
+
+    test('Teste para a funcionalidade discountFromCoupon - Fluxo de erro', async () => {
+      const mockBasket = { update: jest.fn().mockResolvedValue() };
+      BasketModel.findByPk = jest.fn().mockResolvedValue(mockBasket);
+      
+      req.params = { 
+        id: '1', 
+        coupon: 'INVALID_COUPON'
+      }
+  
+      const component = applyCoupon()
+      await component(req, res, next);
+  
+      const result = await error;
+      expect(result).toBe('Invalid coupon.')
+    });
+  })
+
+  describe('[ID: 09] - Tentativa de Aplicação de Cupom em Cesta Inexistente', () => {
+    test('Teste unitário para o método findByPk - Cesta inexistente', async () => {
+      BasketModel.findByPk = jest.fn().mockResolvedValue(null);
+      
+      req.params = { 
+        id: '1', 
+        coupon: security.generateCoupon('10')
+      }
+  
+      const component = applyCoupon()
+      await component(req, res, next);
+  
+      const result = await rejection;
+      expect(result.message).toBe('Basket with id=1 does not exist.')
     });
   })
 })
